bb0571278f7ce69a481f450cb2d3a9463bc10ff6
After Ether <afterether@gmail.com>
2020-06-08 19:18:17 -0500

    repush missing build script

--- auto-build.sh
+++ auto-build.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-GOLANGPATH=$HOME/go
+echo Building ETL daemon ...
+cd etl
+go build ./
 
-echo Getting Golang dependencies for Augur Extractor
-go get -v github.com/gin-gonic/gin
-go get -v github.com/plaid/go-envvar/envvar
-go get -v github.com/stretchr/testify/require
-go get -v github.com/libp2p/go-libp2p-peerstore
-go get -v github.com/jpillora/backoff
-go get -v github.com/0xProject/0x-mesh/zeroex
-go get -v github.com/ethereum/go-ethereum/ethclient
-go get -v github.com/ethereum/go-ethereum/accounts
-go get -v github.com/ethereum/go-ethereum/common
-go get -v github.com/ethereum/go-ethereum/core
-go get -v github.com/lib/pq
-echo All dependencies were downloaded
-echo Getting Augur-Extractor package
-go get -v github.com/afterether/augur-extractor
-cd $GOLANGPATH/src/github.com/afterether/augur-extractor/etl
+echo Building 0x Mesh order listener
+cd mesh
 go build ./
-cd $GOLANGPATH/src/github.com/afterether/augur-extractor/etl/mesh
-go build ./
-cd $GOLANGPATH/src/github.com/afterether/augur-extractor/server
+
+cd ..
+cd ..
+echo Building Web Server ...
+cd server
 go build ./
 
 echo Build complete , now execute: ./auto-run.sh
