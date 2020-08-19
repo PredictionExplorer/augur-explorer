661bd3d8a759fdcafddecd192281ccd44abd1f99
After Ether <afterether@gmail.com>
2020-08-19 10:04:43 -0500

    path reload for mini-supervising scripts

--- etl/dmesh/run-loop.sh
+++ etl/dmesh/run-loop.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 . $HOME/configs/mesh-config.env
 LOG_DIR=$HOME/ae_logs
+WD=`/bin/pwd`
 while true
 do
-	nohup ./dmesh >> $LOG_DIR/mesh_nohup.log
+	nohup $WD/dmesh >> $LOG_DIR/mesh_nohup.log
 	sleep 1
 done
 
