44fdfdb79de76e596f1de49d7a411f6cce0170ac
After Ether <afterether@gmail.com>
2022-10-18 20:57:59 -0500

    fetch balance and pubkey of the validator

--- etl/cmd/beacon/process.go
+++ etl/cmd/beacon/process.go
@@ -37,10 +37,10 @@ func get_head_slot(req_body []byte) int64 {
 		os.Exit(1)
 	}
 	var head_slot int64
-	ead_slot,_ = strconv.ParseInt(head_slot_str,10,64)
+	head_slot,_ = strconv.ParseInt(head_slot_str,10,64)
 	return head_slot
 }
-func get_randao_reveal(req_body []byte) (int64,[]byte) {
+func get_randao_reveal(req_body []byte) (int64,[]byte,string) {
 
 	var d GenericRequestType
 	err := json.Unmarshal(req_body,&d)
@@ -76,7 +76,39 @@ func get_randao_reveal(req_body []byte) (int64,[]byte) {
 	}
 	proposer_index,_ := strconv.ParseInt(proposer_index_str,10,64)
 
-	return proposer_index,randao_reveal
+	state_root,ok := message_iface["state_root"].(string)
+	if !ok {
+		Error.Printf("Can't extract value of 'proposer_index' field")
+		os.Exit(1)
+	}
+
+	return proposer_index,randao_reveal,state_root
+}
+func get_validator_info(req_body []byte) (string,string) {
+	// Returns balance and publike key
+	fmt.Printf("body=%+v\n",string(req_body))
+	var d GenericRequestType
+	err := json.Unmarshal(req_body,&d)
+	if err != nil {
+		Error.Printf("Can't unmarshal JSON: %v\n",err)
+		os.Exit(1)
+	}
+	var ok bool
+	var validator_iface map[string]interface{} 
+	validator_iface,ok = d.Data["validator"].(map[string]interface{})
+	var balance_str string
+	balance_str,ok = d.Data["balance"].(string)
+	if !ok {
+		Error.Printf("Can't extract value of 'balance' field in get_validator_info()")
+		os.Exit(1)
+	}
+	var pubkey_str string
+	pubkey_str,ok = validator_iface["pubkey"].(string)
+	if !ok {
+		Error.Printf("Can't extract value of 'pubkey' field in get_validator_info()")
+		os.Exit(1)
+	}
+	return balance_str,pubkey_str
 }
 func main_event_loop() {
 	url := fmt.Sprintf("%v/eth/v1/node/syncing",API_URL)
@@ -114,8 +146,22 @@ func main_event_loop() {
 			os.Exit(1)
 		}
 		head_slot = head_slot + int64(BLOCK_INCREMENT)
-		proposer_index,randao_reveal := get_randao_reveal(body)
+		proposer_index,randao_reveal,state_id := get_randao_reveal(body)
 		fmt.Printf("Proposer %v\t%v\n",proposer_index,hex.EncodeToString(randao_reveal))
+		url = fmt.Sprintf("%v/eth/v1/beacon/states/%v/validators/%v",API_URL,state_id,proposer_index)
+		fmt.Printf("URL: %v\n",url)
+		resp,err = http.Get(url)
+		if err != nil {
+			Error.Printf("Couldn't successfuly GET at % : %v\n",url,err)
+			os.Exit(1)
+		}
+		body, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			Error.Printf("Error in ReadAll (for validator info): %v\n",err)
+			os.Exit(1)
+		}
+		v_balance,v_pubkey := get_validator_info(body)
+		fmt.Printf("Proposer's balance: %v (pubkey %v)\n",v_balance,v_pubkey)
 		time.Sleep(time.Duration(REQUEST_DELAY) * time.Second)
 	}
 }
