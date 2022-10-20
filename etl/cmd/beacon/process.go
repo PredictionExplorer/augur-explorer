7d7d03d96e87c584e12195c8aea3fccb81b02504
After Ether <afterether@gmail.com>
2022-10-19 21:09:26 -0500

    bugfixes on extractor of Beacon data

--- etl/cmd/beacon/process.go
+++ etl/cmd/beacon/process.go
@@ -40,15 +40,25 @@ func get_head_slot(req_body []byte) int64 {
 	head_slot,_ = strconv.ParseInt(head_slot_str,10,64)
 	return head_slot
 }
-func get_randao_reveal(req_body []byte) (int64,[]byte,string) {
+func get_randao_reveal(req_body []byte) (int64,int64,[]byte,string) {
 
+	var ok bool
+	var check_iface map[string]interface{} 
+	err := json.Unmarshal(req_body,&check_iface)
+	if err == nil {
+		var code_f float64
+		code_f,ok = check_iface["code"].(float64)
+		code := int64(code_f)
+		if ok {
+			return code,0 ,nil,""	// error occured
+		}
+	}
 	var d GenericRequestType
-	err := json.Unmarshal(req_body,&d)
+	err = json.Unmarshal(req_body,&d)
 	if err != nil {
 		Error.Printf("Can't unmarshal JSON: %v\n",err)
 		os.Exit(1)
 	}
-	var ok bool
 	var message_iface map[string]interface{} 
 	message_iface,ok = d.Data["message"].(map[string]interface{})
 	var body map[string]interface{}
@@ -82,18 +92,30 @@ func get_randao_reveal(req_body []byte) (int64,[]byte,string) {
 		os.Exit(1)
 	}
 
-	return proposer_index,randao_reveal,state_root
+	return 0, proposer_index,randao_reveal,state_root
 }
-func get_validator_info(req_body []byte) (string,string) {
-	// Returns balance and publike key
+func get_validator_info(req_body []byte) (int64,string,string) {
+	// Returns request code,balance and publike key
 	fmt.Printf("body=%+v\n",string(req_body))
+
+	var ok bool
+	var check_iface map[string]interface{} 
+	err := json.Unmarshal(req_body,&check_iface)
+	if err == nil {
+		var code_f float64
+		code_f,ok = check_iface["code"].(float64)
+		code := int64(code_f)
+		if ok {
+			return code,"",""	// error occured
+		}
+	}
+	
 	var d GenericRequestType
-	err := json.Unmarshal(req_body,&d)
+	err = json.Unmarshal(req_body,&d)
 	if err != nil {
 		Error.Printf("Can't unmarshal JSON: %v\n",err)
 		os.Exit(1)
 	}
-	var ok bool
 	var validator_iface map[string]interface{} 
 	validator_iface,ok = d.Data["validator"].(map[string]interface{})
 	var balance_str string
@@ -108,9 +130,16 @@ func get_validator_info(req_body []byte) (string,string) {
 		Error.Printf("Can't extract value of 'pubkey' field in get_validator_info()")
 		os.Exit(1)
 	}
-	return balance_str,pubkey_str
+	return 0 ,balance_str,pubkey_str
 }
 func main_event_loop() {
+	fout,err := os.Create("output.csv")
+	if err != nil {
+		fmt.Printf("Error creating file: %v\n",err)
+		os.Exit(1)
+	}
+	defer fout.Close()
+
 	url := fmt.Sprintf("%v/eth/v1/node/syncing",API_URL)
 	resp,err := http.Get(url)
 	if err != nil {
@@ -138,6 +167,7 @@ func main_event_loop() {
 			Error.Printf("Error in ReadAll: %v\n",err)
 			os.Exit(1)
 		}
+		//time.Sleep(time.Duration(7 * time.Second))
 		//fmt.Printf("%+v\n",string(body))
 		var req GenericRequestType
 		err = json.Unmarshal(body,&req)
@@ -146,10 +176,15 @@ func main_event_loop() {
 			os.Exit(1)
 		}
 		head_slot = head_slot + int64(BLOCK_INCREMENT)
-		proposer_index,randao_reveal,state_id := get_randao_reveal(body)
+		req_code,proposer_index,randao_reveal,state_id := get_randao_reveal(body)
+		if req_code !=0 {
+			fmt.Printf("Block not found, skipping\n")
+			continue	// block not found
+		}
 		fmt.Printf("Proposer %v\t%v\n",proposer_index,hex.EncodeToString(randao_reveal))
 		url = fmt.Sprintf("%v/eth/v1/beacon/states/%v/validators/%v",API_URL,state_id,proposer_index)
 		fmt.Printf("URL: %v\n",url)
+	again:
 		resp,err = http.Get(url)
 		if err != nil {
 			Error.Printf("Couldn't successfuly GET at % : %v\n",url,err)
@@ -160,8 +195,16 @@ func main_event_loop() {
 			Error.Printf("Error in ReadAll (for validator info): %v\n",err)
 			os.Exit(1)
 		}
-		v_balance,v_pubkey := get_validator_info(body)
+		code,v_balance,v_pubkey := get_validator_info(body)
+		if code != 0 {
+			fmt.Printf("Error %v, sleeping for 1 sec\n",code)
+			time.Sleep(time.Duration(1 * time.Second))
+			fmt.Printf("Retrying url %v\n",url)
+			goto again
+		}
 		fmt.Printf("Proposer's balance: %v (pubkey %v)\n",v_balance,v_pubkey)
-		time.Sleep(time.Duration(REQUEST_DELAY) * time.Second)
+		fout.WriteString(fmt.Sprintf("%v\t%v\t%v\t%v\t%v\n",head_slot,proposer_index,hex.EncodeToString(randao_reveal),v_balance,v_pubkey))
+		//time.Sleep(time.Duration(REQUEST_DELAY) * time.Second)
+		//os.Exit(1)
 	}
 }
