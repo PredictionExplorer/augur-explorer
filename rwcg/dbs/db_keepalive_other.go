8da65c52c8d671e870135bbc37345ab91d133b75
AfterEther <afterether@gmail.com>
2026-06-25 19:49:06 -0600

    missing files push

---
+++ rwcg/dbs/db_keepalive_other.go
@@ -0,0 +1,14 @@
+//go:build !linux
+
+package dbs
+
+import "syscall"
+
+// Non-Linux platforms: rely on net.Dialer's built-in keepalive (SO_KEEPALIVE
+// with this idle/interval). TCP_KEEPIDLE / TCP_KEEPCNT / TCP_USER_TIMEOUT are
+// not portable, so they're skipped here.
+var dialerKeepAlive = dbKeepaliveIdle
+
+func tcpKeepaliveControl(_, _ string, _ syscall.RawConn) error {
+	return nil
+}
